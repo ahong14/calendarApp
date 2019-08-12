215922573bf63a77adfeec465121137e9ec03d1a
Alex Hong <alexhong681@gmail.com>
2019-08-11 22:24:56 -0700

    setting up auth token for login

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,8 @@ var sequelize = require('../sequelize');
 var validator = require('validator');
 var bcrypt = require('bcrypt');
 var uniqid = require('uniqid');
+var jwt = require('jsonwebtoken');
+
 
 var User = sequelize.models.User;
 const saltRounds = 10;
@@ -30,7 +32,7 @@ router.post('/signup', (req, res) => {
 
   //check for valid email
   if(validator.isEmail(userEmail) === false){
-    res.status(200).json({
+    return res.status(200).json({
       success: false,
       message:"Invalid email"
     })
@@ -39,7 +41,7 @@ router.post('/signup', (req, res) => {
   //check for valid password length
   if(validator.isLength(userPassword, {min: 7,max: 20}) === false)
   {
-    res.status(200).json({
+    return res.status(200).json({
       success: false,
       message:"Password must be minimum 8 characters, max 20 characters"
     })
@@ -61,7 +63,7 @@ router.post('/signup', (req, res) => {
       password: hash
     })
     .then(user => {
-      res.status(200).json({
+      return res.status(200).json({
         success: true,
         message: "User created!"
       })
@@ -83,5 +85,83 @@ router.get('/getUsers', (req, res) => {
 })
 
 //login user
+router.post('/login', (req, res) => {
+  var userEmail = "";
+  var userPassword = "";
+
+  //extract email/password params from request
+  if(req.body.params){
+    userEmail = req.body.params.email;
+    userPassword = req.body.params.password;
+  }
+
+  else if(req.body){
+    userEmail = req.body.email;
+    userPassword = req.body.password;
+  }
+
+  User.findOne({
+    where: {
+      email: userEmail
+    },
+    raw: true
+  }).then( user => {
+    if(user){
+      console.log("user found: ", user);
+      
+      bcrypt.compare(userPassword, user.password, (err, password) => {
+        if(err){
+          return res.status(200).json({
+            success: false,
+            message: "Error comparing passwords"
+          })
+        }
+
+        else if (password === false){
+          return res.status(200).json({
+            success: false,
+            message: "Passwords do not match"
+          })
+        }
+
+        //passwords match
+        console.log("passwords match");
+
+        //create JWT for successful login
+        jwt.sign({
+          data: {
+            email: user.email
+          }},
+          process.env.JWT_SECRET,
+          { expiresIn: 3600 },
+          (err, token) => {
+            if(err){
+              return res.status(200).json({
+                success: false,
+                message: "Error signing token"
+              })
+            }
+
+            //send back auth token
+            return res.status(200).json({
+              success: true,
+              message: "Login successful",
+              token: token
+            })
+          } 
+          );
+      })
+    }
+
+    else{
+      return res.status(200).json({
+        success: false,
+        message: "User doesn't exist"
+      })
+    }
+  }) //end of finding one user from database
+
+
+})
 
 module.exports = router;
